d6548984018efa83da32b40832d065feb2ac8458
Christin Boner <christinmclemons@gmail.com>
2019-09-07 14:00:45 -0700

    adding initial files and linked stylesheets

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+//TITLE SCREEN--------------------------
+//start button--
+//on click,, hide the start button
+//on click show the game div
+//if/when the game div get shown, start the timer gameDivVisible=true
+//GAME DIV------------------
+//each question shows in its own div with a radio for each answer.
+//each question will stored in an array
+//each set of answer will be stored as an object within each question
+//when the answer is chosen it records the answer and logs it into the calcScore function
+//if/when the timer runs out OR the done button get clicked, 
+//gameDivVisible=false, and calcScore to show in scoreDivVisible=true
+//
+alert("Hello World!")
+
